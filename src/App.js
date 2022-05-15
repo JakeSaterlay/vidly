0e33c0abb086a6f589feb2e201c580d1333c468c
Jake Saterlay <jake@vocala.co>
2022-05-15 12:45:55 +0100

    added routing examples

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
-import "./App.css";
 import Movies from "./components/movies";
+import Customers from "./components/customers";
+import Rentals from "./components/rentals";
+import NotFound from "./components/notFound";
+import MovieForm from "./components/movieForm";
+import { Route, Routes, Navigate } from "react-router-dom";
+import NavBar from "./components/navBar";
+import "./App.css";
+import React from "react";
 
 function App() {
 	return (
-		<main className="container">
-			<Movies />
-		</main>
+		<React.Fragment>
+			<NavBar />
+			<main className="container">
+				<Routes>
+					<Route path="/movies/:id" element={<MovieForm />}></Route>
+					<Route path="/movies" element={<Movies />}></Route>
+					<Route path="/customers" element={<Customers />}></Route>
+					<Route path="/rentals" element={<Rentals />}></Route>
+					<Route path="/not-found" element={<NotFound />}></Route>
+					<Route path="/" element={<Navigate replace to="/movies" />} />
+					<Route path="*" element={<NotFound />} />
+				</Routes>
+			</main>
+		</React.Fragment>
 	);
 }
 
