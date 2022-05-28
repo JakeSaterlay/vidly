a97c39629d80a316acc8acb084433a2d9d174234
Jake Saterlay <jake@vocala.co>
2022-05-28 14:05:25 +0100

    pulled down example with correct dependencies

--- src/App.js
+++ src/App.js
@@ -1,32 +1,36 @@
+import React, { Component } from "react";
+import { Route, Redirect, Switch } from "react-router-dom";
 import Movies from "./components/movies";
+import MovieForm from "./components/movieForm";
 import Customers from "./components/customers";
 import Rentals from "./components/rentals";
 import NotFound from "./components/notFound";
-import MovieForm from "./components/movieForm";
-import { Route, Routes, Navigate } from "react-router-dom";
 import NavBar from "./components/navBar";
-import React from "react";
 import LoginForm from "./components/loginForm";
+import RegisterForm from "./components/registerForm";
 import "./App.css";
 
-function App() {
-	return (
-		<React.Fragment>
-			<NavBar />
-			<main className="container">
-				<Routes>
-					<Route path="/login" element={<LoginForm />}></Route>
-					<Route path="/movies/:id" element={<MovieForm />}></Route>
-					<Route path="/movies" element={<Movies />}></Route>
-					<Route path="/customers" element={<Customers />}></Route>
-					<Route path="/rentals" element={<Rentals />}></Route>
-					<Route path="/not-found" element={<NotFound />}></Route>
-					<Route path="/" element={<Navigate replace to="/movies" />} />
-					<Route path="*" element={<NotFound />} />
-				</Routes>
-			</main>
-		</React.Fragment>
-	);
+class App extends Component {
+  render() {
+    return (
+      <React.Fragment>
+        <NavBar />
+        <main className="container">
+          <Switch>
+            <Route path="/register" component={RegisterForm} />
+            <Route path="/login" component={LoginForm} />
+            <Route path="/movies/:id" component={MovieForm} />
+            <Route path="/movies" component={Movies} />
+            <Route path="/customers" component={Customers} />
+            <Route path="/rentals" component={Rentals} />
+            <Route path="/not-found" component={NotFound} />
+            <Redirect from="/" exact to="/movies" />
+            <Redirect to="/not-found" />
+          </Switch>
+        </main>
+      </React.Fragment>
+    );
+  }
 }
 
 export default App;
