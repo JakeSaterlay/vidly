e415098317d33fbfff69b91ac2c9a69165e0e084
Jake Saterlay <jake@vocala.co>
2022-06-03 18:10:47 +0100

    integrating vidly with mongodb

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { Route, Redirect, Switch } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
 import Movies from "./components/movies";
 import MovieForm from "./components/movieForm";
 import Customers from "./components/customers";
@@ -9,28 +10,30 @@ import NavBar from "./components/navBar";
 import LoginForm from "./components/loginForm";
 import RegisterForm from "./components/registerForm";
 import "./App.css";
+import "react-toastify/dist/ReactToastify.css";
 
 class App extends Component {
-  render() {
-    return (
-      <React.Fragment>
-        <NavBar />
-        <main className="container">
-          <Switch>
-            <Route path="/register" component={RegisterForm} />
-            <Route path="/login" component={LoginForm} />
-            <Route path="/movies/:id" component={MovieForm} />
-            <Route path="/movies" component={Movies} />
-            <Route path="/customers" component={Customers} />
-            <Route path="/rentals" component={Rentals} />
-            <Route path="/not-found" component={NotFound} />
-            <Redirect from="/" exact to="/movies" />
-            <Redirect to="/not-found" />
-          </Switch>
-        </main>
-      </React.Fragment>
-    );
-  }
+	render() {
+		return (
+			<React.Fragment>
+				<ToastContainer />
+				<NavBar />
+				<main className="container">
+					<Switch>
+						<Route path="/register" component={RegisterForm} />
+						<Route path="/login" component={LoginForm} />
+						<Route path="/movies/:id" component={MovieForm} />
+						<Route path="/movies" component={Movies} />
+						<Route path="/customers" component={Customers} />
+						<Route path="/rentals" component={Rentals} />
+						<Route path="/not-found" component={NotFound} />
+						<Redirect from="/" exact to="/movies" />
+						<Redirect to="/not-found" />
+					</Switch>
+				</main>
+			</React.Fragment>
+		);
+	}
 }
 
 export default App;
